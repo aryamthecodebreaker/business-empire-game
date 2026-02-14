9a1dbf1f4580d9ae92a115b7b629b4027ea6980e
aryamthecodebreaker <130867585+aryamthecodebreaker@users.noreply.github.com>
2026-02-14 18:12:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Client-side configuration
 // Supabase credentials — replace with your project's values
-window.SUPABASE_URL = 'https://kvseyoncmoksrrbzbceo.supabase.co';
+window.SUPABASE_URL = 'https://jztzbcaebkgoykjslgdt.supabase.co';
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2c2V5b25jbW9rc3JyYnpiY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwMTE2MDEsImV4cCI6MjA4NjU4NzYwMX0.iWWLcnjtuPaVWv0YGEAr_4WcmJeL58js2u4TtHoQAT0';
 
 // API server URL — not used for GitHub Pages
